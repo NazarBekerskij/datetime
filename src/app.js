d70470762a8a5bfca0dc8636425f8663c966d06c
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-03-22 09:49:33 +0200

    save-data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
-const qwe = new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
-});
+const day = document.querySelector('[data-value="days"]');
+const hours = document.querySelector('[data-value="hours"]');
+const minute = document.querySelector('[data-value="mins"]');
+const second = document.querySelector('[data-value="secs"]');
 
+const happyDays = new Date(2027, 0, 1, 0, 0, 0);
 
+setInterval(() => {
+  const currentDate = new Date(); 
+  const time = (happyDays - currentDate);
+
+  const day1 = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours1 = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+  day.textContent = day1;
+  hours.textContent = hours1;
+  minute.textContent = mins;
+  second.textContent = secs;
+}, 1000);
