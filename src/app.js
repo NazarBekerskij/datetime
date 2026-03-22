cdb62f3922ad47cc165a5ae3b836091c29d59b4a
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-03-22 10:48:54 +0200

    save timer solo hepy days

--- src/app.js
+++ src/app.js
@@ -3,19 +3,31 @@ const hours = document.querySelector('[data-value="hours"]');
 const minute = document.querySelector('[data-value="mins"]');
 const second = document.querySelector('[data-value="secs"]');
 
-const happyDays = new Date(2027, 0, 1, 0, 0, 0);
+const happeDay = new Date(2026, 9,30,7,30,0)
 
-setInterval(() => {
-  const currentDate = new Date(); 
-  const time = (happyDays - currentDate);
 
-  const day1 = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours1 = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
+const id = setInterval(() => {
 
-  day.textContent = day1;
-  hours.textContent = hours1;
-  minute.textContent = mins;
-  second.textContent = secs;
-}, 1000);
+
+const currenDay = new Date()
+const time = happeDay - currenDay 
+  
+
+if(time < 0){
+  clearInterval(id)
+  alert("З ДНЕМ НАРОДЖЕННЯ!!!")
+  document.body.style.backgroundColor = "gold";
+  return;
+}
+
+
+const daysLeft = Math.floor(time / (1000 * 60 * 60 * 24));
+const hours1 = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+day.textContent = daysLeft;
+hours.textContent = hours1;
+minute.textContent = mins;
+second.textContent = secs;
+},1000)
